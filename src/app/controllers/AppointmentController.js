b851339f0edbd70e32531de782782660ec3673a9
Christian Fleury <chfleurysiq@gmail.com>
2021-01-06 19:06:46 -0300

    adding nodemailer

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,9 +5,10 @@ import Appointment from "../models/Appointment";
 import User from "../models/User";
 import File from "../models/File";
 import Notification from "../schemas/Notification";
+import Mail from "../../lib/Mail";
 class AppointmentsController {
   async index(req, res) {
-    const { page = 1 } = req.qeury;
+    const { page = 1 } = req.query;
 
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
@@ -117,7 +118,15 @@ class AppointmentsController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: "provider",
+          attributes: ["name", "email"],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -138,7 +147,18 @@ class AppointmentsController {
      * Updating canceled_at field
      */
     appointment.canceled_at = new Date();
-    appointment.save();
+
+    await appointment.save();
+
+    try {
+      await Mail.sendMail({
+        to: `${appointment.provider.name} <${appointment.provider.email}>`,
+        subject: "Agendamento cancelado",
+        text: "Você tem um novo cancelamento",
+      });
+    } catch (err) {
+      console.log(err);
+    }
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailspons.com",
+  port: "2525",
+  secure: false,
+  auth: {
+    user: "da530b2273cc4654b8b3",
+    pass: "394de4fd7ceb4e2bb285bad2b136fb10",
+  },
+  default: {
+    from: "Equipe GoBarber <noreply@gobarber.com>",
+  },
+};
